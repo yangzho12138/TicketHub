bf7301b1302c9fbd895d1884a12a919ce0a85940
yz96 <68858113+yangzho12138@users.noreply.github.com>
2022-12-27 12:29:42 -0600

    login auth

--- auth/src/routers/signin.ts
+++ auth/src/routers/signin.ts
@@ -1,22 +1,17 @@
 import express, { Request, Response } from 'express'
-import { body, validationResult } from 'express-validator'
-import { RequestValidationError } from '../errors/request-validation-error';
+import { body } from 'express-validator'
 import jwt from 'jsonwebtoken'
 import { User } from '../models/users';
 import { BadRequestError } from '../errors/bad-request-error';
 import { Password } from '../utils/password';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
 router.post('/api/users/signin', [
     body('email').isEmail().withMessage('Email must be valid'),
     body('password').trim().notEmpty().withMessage('Password can not be empty')
-],async (req : Request, res : Response) => {
-    const errors = validationResult(req)
-    if(!errors.isEmpty()){
-        throw new RequestValidationError(errors.array())
-    }
-
+], validateRequest, async (req : Request, res : Response) => {
     const { email, password } = req.body
 
     const existingUser = await User.findOne({ email })
--- auth/src/routers/signup.ts
+++ auth/src/routers/signup.ts
@@ -1,9 +1,9 @@
 import express, { Request, Response } from 'express'
-import { body, validationResult } from 'express-validator'
-import { RequestValidationError } from '../errors/request-validation-error';
+import { body } from 'express-validator'
 import { User } from '../models/users';
 import { BadRequestError } from '../errors/bad-request-error';
 import jwt from 'jsonwebtoken'
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
@@ -11,12 +11,7 @@ router.post('/api/users/signup', [
     // if there is something wrong, errors will be appended to the req, and use validationResult can get errors
     body('email').isEmail().withMessage('Email must be valid'),
     body('password').trim().isLength({ min: 4, max: 20}).withMessage('Password is invalid')
-],async (req : Request, res : Response) => {
-    const errors = validationResult(req)
-    if(!errors.isEmpty()){
-        throw new RequestValidationError(errors.array())
-    }
-
+], validateRequest,async (req : Request, res : Response) => {
     const { email, password } = req.body
     
     const existingUser = await User.findOne({email})

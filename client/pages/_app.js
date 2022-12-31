69bca1198b9abdf29833d2ccc7c8769662d9e0a8
yz96 <68858113+yangzho12138@users.noreply.github.com>
2022-12-31 12:12:10 -0600

    signin signup

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,27 @@
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css' // global css
+import buildClient from '../api/build-client'
 
-const app = ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps, currentUser }) => {
     return <Component {...pageProps} />
 }
 
-export default app
+// appContext is different the context in an individual page: appContext.ctx = context
+AppComponent.getInitProps = async(appContext) => {
+    const client = buildClient(appContext.ctx)
+    const { data } = await client.get('/api/users/currentuser')
+
+    // getInitProps of an individual page will not be invoked automatically is there is a getInitProps on _app.js
+    // manually call the individual page getInitProps 
+    // some pages may not have getInitProps --> check
+    let pageProps = {}
+    if(appContext.Component.getInitProps){
+        pageProps = await appContext.Component.getInitProps(appContext.ctx)
+    }
+
+    return {
+        pageProps,
+        ...data
+    }
+}
+
+export default AppComponent
--- client/pages/auth/signin.js
+++ client/pages/auth/signin.js
@@ -2,17 +2,15 @@ import { useState } from "react";
 import Link from "next/link";
 import useRequest from "../../hooks/use-request";
 
-const signup = () => {
+const Signin = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [confirmedPassword, setConfirmedPassword] = useState("");
     const { doRequest, errors } = useRequest({
-        url: "/api/users/signup",
+        url: "/api/users/signin",
         method: "post",
         body: {
           email,
           password,
-          confirmedPassword
         },
         onSuccess: () => Router.push("/"),
       });
@@ -23,37 +21,35 @@ const signup = () => {
         
         await doRequest()
     }
-    return (
+
+
+    return(
         <div style={{backgroundImage: `url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQe_sXx7aiSMXPaq2YlEpBfkBiw4IM5qPeV7Q&usqp=CAU)`, width: '100vw', height: '100vh', backgroundSize: 'cover', backgroundRepeat: 'no-repeat', display: 'flex' }}>
             <div className="card" style={{width: '30vw', height: '60vh', marginLeft: '10vw', alignSelf: 'center'}}>
                 <div className="card-body" style={{display: 'flex'}}>
                     <div style={{alignSelf: 'center', margin: 'auto'}}>
-                        <h2 className="card-title">Sign up</h2>
+                        <h2 className="card-title">Sign in</h2>
                         <form onSubmit={onSubmit}>
                         <div className="mb-3">
                             <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
                             <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={email} onChange={(e) => setEmail(e.target.value)} />
-                            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                         </div>
                         <div className="mb-3">
                             <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
                             <input type="password" className="form-control" id="exampleInputPassword1" value={password} onChange={(e) => setPassword(e.target.value)}/>
                         </div>
                         <div className="mb-3">
-                            <label htmlFor="exampleInputPassword2" className="form-label">Confirm Password</label>
-                            <input type="password" className="form-control" id="exampleInputPassword2" value={confirmedPassword} onChange={(e) => setConfirmedPassword(e.target.value)}/>
-                        </div>
-                        <div className="mb-3">
-                            <Link href="/auth/signin">Have an account? Signin now!</Link>
+                            <Link href="/auth/signup">Do not have an account? Register!</Link>
                         </div>
                         {errors}
-                        <button type="submit" className="btn btn-primary">Signip</button>
+                        <button type="submit" className="btn btn-primary">Signin</button>
                         </form>
                     </div>
                 </div>
             </div>
         </div>
     )
+
 }
 
-export default signup
+export default Signin

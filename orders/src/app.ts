cc68de416ae023b99eeb212eb602d4e6fb1129ae
yz96 <68858113+yangzho12138@users.noreply.github.com>
2023-01-05 21:59:24 -0600

    order config

---
+++ orders/src/app.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+import { json } from 'body-parser'
+import 'express-async-errors' // 可以在async函数中throw error
+import cookieSession from 'cookie-session'
+import { NotFoundError, errorHandler, currentUser } from '@ticket_hub/common';
+
+const app = express()
+app.set('trust proxy', true) // https
+app.use(json())
+app.use(cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test' // need https request (test env: false -> http)
+}))
+app.use(currentUser) // get token from cookie and wirte userinfo into req
+
+
+app.all('*', (req, res) => {
+    throw new NotFoundError()
+})
+
+app.use(errorHandler)
+
+export { app }

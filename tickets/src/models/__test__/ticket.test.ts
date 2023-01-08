37b24f99676cd6426e94a58633f8e279af833957
yz96 <68858113+yangzho12138@users.noreply.github.com>
2023-01-08 14:12:08 -0600

    ticket model test

---
+++ tickets/src/models/__test__/ticket.test.ts
@@ -0,0 +1,46 @@
+import { Ticket } from "../ticket";
+
+it('concurrency problem - version control', async() => {
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 100,
+        number: 100,
+        userId: 'test'
+    })
+
+    await ticket.save()
+
+    const firstInstance = await Ticket.findById(ticket.id)
+    const secondInstance = await Ticket.findById(ticket.id)
+
+    // make two separate changes to the ticket
+    firstInstance!.set({ price: 10 })
+    secondInstance!.set({ price: 15 })
+
+    // version update
+    await firstInstance!.save()
+
+    try{
+        await secondInstance!.save()
+    }catch(err){
+        return ;
+    }
+
+    throw new Error('Should not reach this point')
+})
+
+it('increase the version number on multiple saves', async() => {
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 100,
+        number: 100,
+        userId: 'test'
+    })
+
+    await ticket.save()
+    expect(ticket.version).toEqual(0)
+    await ticket.save()
+    expect(ticket.version).toEqual(1)
+    await ticket.save()
+    expect(ticket.version).toEqual(2)
+})
